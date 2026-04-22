2ce67cc6b31e6d24022519340e6850128e95a4d8
ferrles124 <mehmetemredursun9@gmail.com>
2026-04-23 01:24:13 +0300

    Update NPC_CheckAction_Patch.cs

--- src/Patches/NPC_CheckAction_Patch.cs
+++ src/Patches/NPC_CheckAction_Patch.cs
@@ -1,1 +1,25 @@
+using HarmonyLib;
+using StardewValley;
 
+namespace MedTalk
+{
+    [HarmonyPatch(typeof(NPC), nameof(NPC.checkAction))]
+    public class NPC_CheckAction_Patch
+    {
+        public static bool Prefix(ref NPC __instance, ref bool __result, Farmer who, GameLocation l)
+        {
+            if (__instance.IsInvisible ||
+                !who.CanMove ||
+                !DialogueBuilder.Instance.PatchNpc(__instance))
+            {
+                return true;
+            }
+
+            DialogueBuilder.Instance.ClearContext();
+            TextInputManager.RequestTextInput($"What do you want to say to {__instance.displayName}?", __instance);
+
+            __result = false;
+            return false;
+        }
+    }
+}
